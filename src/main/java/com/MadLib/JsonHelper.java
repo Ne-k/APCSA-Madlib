fe53038a909ca3f1f549a5007e146d8afd7e2a75
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-09-19 08:34:47 -0700

    updated JsonHelper.java

--- src/main/java/com/MadLib/JsonHelper.java
+++ src/main/java/com/MadLib/JsonHelper.java
@@ -26,34 +26,4 @@ public class JsonHelper {
             return new JSONObject(jsonText);
         }
     }
-
-    public static void request(String boner, JSONObject data) throws IOException {
-        URL url = new URL(String.format("%s", boner));
-
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("POST");
-
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("Accept", "application/json");
-        con.setDoOutput(true);
-
-        String jsonInputString = data.toString();
-
-        try(OutputStream os = con.getOutputStream()) {
-            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
-            os.write(input, 0, input.length);
-        }
-
-        try(BufferedReader br = new BufferedReader(
-                new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
-            StringBuilder response = new StringBuilder();
-            String responseLine = null;
-            while ((responseLine = br.readLine()) != null) {
-                response.append(responseLine.trim());
-            }
-            System.out.println(response.toString());
-        }
-
-
-    }
 }
